6d3dfb65e15799922758bd79649dee3e056bd57e
Louise Cuthbertson <lcuthbertson_3112@hotmail.co.uk>
2023-05-01 16:12:33 +0100

    task completed

--- src/App.test.js
+++ src/App.test.js
@@ -4,10 +4,10 @@ import ReactDOM from 'react-dom';
 import App from './App';
 
 describe('App', () => {
-  it('renders without crashing', () => {
-    const div = document.createElement('div');
+    it('renders without crashing', () => {
+        const div = document.createElement('div');
 
-    ReactDOM.render(<App />, div);
-    ReactDOM.unmountComponentAtNode(div);
-  });
+        ReactDOM.render(<App />, div);
+        ReactDOM.unmountComponentAtNode(div);
+    });
 });
